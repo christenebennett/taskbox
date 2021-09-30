3021c5d207b9dc3703bb2842cd9d53d654b0f998
Christene Bennett <cbennett@MAC-C02G47GLQ05P.local>
2021-09-30 11:38:55 -0700

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Task({ task: {id, title, state}, onArchiveTask, onPinTask}) {
-    return (
-        <div className={`list-item ${state}`}>
+export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+  return (
+    <div className={`list-item ${state}`}>
       <label className="checkbox">
         <input
           type="checkbox"
@@ -14,7 +14,7 @@ export default function Task({ task: {id, title, state}, onArchiveTask, onPinTas
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        <input type="text" value={title} readOnly={true} placeholder="Input title" style={{ background: 'red' }} />
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
@@ -26,15 +26,15 @@ export default function Task({ task: {id, title, state}, onArchiveTask, onPinTas
         )}
       </div>
     </div>
-    )
+  )
 }
 
 Task.propTypes = {
-    task: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        state: PropTypes.string.isRequired,
-    }),
-    onArchiveTask: PropTypes.func,
-    onPinTask: PropTypes.func,
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
 }
